73f6e134e860b127ecac72e0d4f25ff3fa37910d
JAVIERA INOSTROZA <jminostroza@uc.cl>
2020-11-18 18:31:22 -0300

    send play with room id

--- routes.js
+++ routes.js
@@ -7,6 +7,7 @@ const users = require('./routes/users');
 const rooms = require('./routes/rooms');
 const auth = require('./routes/auth');
 const map = require('./routes/map')
+const play = require('./routes/plays');
 
 
 router.use('/orders', orders.routes());
@@ -14,6 +15,7 @@ router.use('/users', users.routes());
 router.use('/rooms', rooms.routes());
 router.use('/auth', auth.routes());
 router.use('/map', map.routes());
+router.use('/play', play.routes());
 
 
 module.exports = router;
